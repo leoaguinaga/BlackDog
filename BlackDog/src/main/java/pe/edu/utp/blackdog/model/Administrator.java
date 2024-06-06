9eeab405a315151f17b0bb4429d46ef45cd946a0
Leo <u21219471@utp.edu.pe>
2024-06-06 03:21:12 -0500

    half models

--- BlackDog/src/main/java/pe/edu/utp/blackdog/model/Administrator.java
+++ BlackDog/src/main/java/pe/edu/utp/blackdog/model/Administrator.java
@@ -6,46 +6,38 @@ public class Administrator {
     private String email;
     private String pwd;
 
-    public Administrator() {
+    public Administrator(Builder builder) {
     }
 
-    public Administrator(long admin_id, String full_name, String email, String pwd) {
-        this.admin_id = admin_id;
-        this.full_name = full_name;
-        this.email = email;
-        this.pwd = pwd;
+    //INNER CLASS: BUILDER
+    public static class Builder{
+        private long admin_id;
+        private String full_name;
+        private String email;
+        private String pwd;
+
+        public Builder(long admin_id, String full_name, String email, String pwd){
+            this.admin_id = admin_id;
+            this.full_name = full_name;
+            this.email = email;
+            this.pwd = pwd;
+        }
+
+        public Administrator build(){return new Administrator(this);}
     }
 
+    // GETTERS
     public long getAdmin_id() {
         return admin_id;
     }
-
-    public void setAdmin_id(long admin_id) {
-        this.admin_id = admin_id;
-    }
-
     public String getFull_name() {
         return full_name;
     }
-
-    public void setFull_name(String full_name) {
-        this.full_name = full_name;
-    }
-
     public String getEmail() {
         return email;
     }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getPwd() {
         return pwd;
-    }
-
-    public void setPwd(String pwd) {
-        this.pwd = pwd;
     }
 
     @Override
--- BlackDog/src/main/java/pe/edu/utp/blackdog/model/Client.java
+++ BlackDog/src/main/java/pe/edu/utp/blackdog/model/Client.java
@@ -11,13 +11,49 @@ public class Client {
     public Client() {
     }
 
-    public Client(long client_id, String first_name, String last_name, String phone_number, String email, String pwd) {
-        this.client_id = client_id;
-        this.first_name = first_name;
-        this.last_name = last_name;
-        this.phone_number = phone_number;
-        this.email = email;
-        this.pwd = pwd;
+    public Client(Builder builder) {
+    }
+
+    //INNER CLASS: BUILDER
+    public static class Builder{
+        private long client_id;
+        private String first_name;
+        private String last_name;
+        private String phone_number;
+        private String email;
+        private String pwd;
+
+        public Builder(long client_id, String first_name, String last_name, String phone_number, String email, String pwd){
+            this.client_id = client_id;
+            this.first_name = first_name;
+            this.last_name = last_name;
+            this.phone_number = phone_number;
+            this.email = email;
+            this.pwd = pwd;
+        }
+
+        public Client build(){return new Client(this);}
+    }
+
+    // GETTERS
+    public long getClient_id() {
+        return client_id;
+    }
+    public String getFirst_name() {
+        return first_name;
+    }
+    public String getLast_name() {
+        return last_name;
+    }
+    public String getPhone_number() {
+        return phone_number;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPwd() {
+        return pwd;
     }
 
     @Override
@@ -30,53 +66,5 @@ public class Client {
                 ", email='" + email + '\'' +
                 ", pwd='" + pwd + '\'' +
                 '}';
-    }
-
-    public long getClient_id() {
-        return client_id;
-    }
-
-    public void setClient_id(long client_id) {
-        this.client_id = client_id;
-    }
-
-    public String getFirst_name() {
-        return first_name;
-    }
-
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
-    }
-
-    public String getLast_name() {
-        return last_name;
-    }
-
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
-    }
-
-    public String getPhone_number() {
-        return phone_number;
-    }
-
-    public void setPhone_number(String phone_number) {
-        this.phone_number = phone_number;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPwd() {
-        return pwd;
-    }
-
-    public void setPwd(String pwd) {
-        this.pwd = pwd;
     }
 }
